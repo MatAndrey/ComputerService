f74b338941fb952fd57e5c7686415b3791b90f71
Andrey Matyushin <andrey.matyushin05@gmail.com>
2026-04-10 11:13:18 +0300

    Добавьте файлы проекта.

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ComputerService.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ComputerService.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
